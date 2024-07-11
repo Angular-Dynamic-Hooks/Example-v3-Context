023a5941b9d4510c08c0e291536010e4249b2a83
Marvin <Mvin@live.de>
2024-07-11 15:56:57 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DynamicHooksComponent } from 'ngx-dynamic-hooks';
+import { ExampleComponent } from './components/example/example.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [DynamicHooksComponent, ExampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'general-context';
+  content = 'Load a component here: <app-example [message]="context.someString"></app-example>';
+  components = [ExampleComponent];
+  context = {someString: "Greetings from the context object!"};
 }
